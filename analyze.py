94af221d5abc45a994e5ed1fb89994e7361fe889
DenmanBlanchard <Denman.Blanchard1@outlook.com>
2026-06-20 15:31:31 -0400

    added get wins function

--- analyze.py
+++ analyze.py
@@ -1,5 +1,21 @@
+import json
+
+import pandas as pd
+
 from parsedata import main_parse
+
+
+def getWins(teamid, matchday):
+
+    with open(
+        os.path.join(os.path.abspath("data\\teams"), f"{teamid}.json"), "r"
+    ) as file:
+        matches = json.load(file)
+        df = pd.DataFrame.from_dict(matches["matches"])
+
+        print(df)
 
 
 def main_analyze(api_key):
     main_parse()
+    getWins()
