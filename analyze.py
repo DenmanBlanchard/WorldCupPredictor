ebbe62fd61b3803a3d50a0756f176ce6b6d2d3ea
Denman Blanchard <Denman.Blanchard1@outlook.com>
2026-06-20 20:48:56 -0400

    implemented win conditions

--- analyze.py
+++ analyze.py
@@ -3,20 +3,80 @@ import os
 
 import pandas as pd
 
-from parsedata import main_parse
+startElo = 10000
+eloDF = pd.DataFrame(columns=["id", "elo"])
 
 
-def getWins(teamid, matchday):
+def updateTeamElo(team1id, team2id=0, wld=0, init=False):
+    if init:
+        eloDF.loc[len(eloDF)] = [team1id, startElo]
+    else:
+        match wld:
+            case 0:  # They won
+                t1EloDF = eloDF[eloDF["id"] == team1id]
+                t2EloDF = eloDF[eloDF["id"] == team2id]
+
+                if t1EloDF.iat[0, 1] > t2EloDF.iat[0, 1]:
+                    eloDF.loc[eloDF.iloc[:, 0] == team1id, "elo"] = (
+                        t1EloDF.at[0, "elo"] + 100
+                    )
+                elif t1EloDF.iat[0, 1] == t2EloDF.iat[0, 1]:
+                    eloDF.loc[eloDF.iloc[:, 0] == team1id, "elo"] = t1EloDF.iat[
+                        0, 1
+                    ] + (t2EloDF.iat[0, 1] / 2)
+                elif t1EloDF.iat[0, 1] < t2EloDF.iat[0, 1]:
+                    eloDF.loc[eloDF.iloc[:, 0] == team1id, "elo"] = t1EloDF.iat[
+                        0, 1
+                    ] + ((t2EloDF.iat[0, 1] / 4) * 3)
+                else:
+                    raise ValueError
+
+            case 1:  # They lost
+                pass
+            case 2:  # They drew
+                pass
+
+
+def updateElo(teamid, matchday):
 
     with open(
-        os.path.join(os.path.abspath("data\\teams"), f"{teamid}.json"), "r"
+        os.path.join(os.path.abspath("data"), "teams", f"{teamid}.json"), "r"
     ) as file:
         matches = json.load(file)
-        df = pd.DataFrame.from_dict(matches["matches"])
 
-        print(df)
+        for match in matches["matches"]:
+            if match["matchday"] == matchday:
+                if match["status"] == "FINISHED":
+                    match match["score"]["winner"]:
+                        case "AWAY_TEAM":
+                            if match["awayTeam"]["id"] == teamid:
+                                updateTeamElo(teamid, match["homeTeam"]["id"], 0)
+                            else:
+                                updateTeamElo(teamid, match["homeTeam"]["id"], 1)
+                        case "HOME_TEAM":
+                            if match["homeTeam"]["id"] == teamid:
+                                updateTeamElo(teamid, match["awayTeam"]["id"], 0)
+                            else:
+                                updateTeamElo(teamid, match["awayTeam"]["id"], 1)
+                        case "DRAW":
+                            updateTeamElo(
+                                match["awayTeam"]["id"], match["homeTeam"]["id"], 2
+                            )
+                else:
+                    pass
 
 
 def main_analyze(api_key):
-    main_parse()
-    getWins()
+    # main_parse()
+    with open(os.path.join(os.path.abspath("data"), "teams.json"), "r") as file:
+        teams = json.load(file)
+        # Initialize elo
+        for team in teams["teams"]:
+            updateTeamElo(team["id"], init=True)
+
+        # Update elo
+        for team in teams["teams"]:
+            for matchday in range(teams["season"]["currentMatchday"] - 1):
+                updateElo(team["id"], matchday + 1)
+
+        print(eloDF)
